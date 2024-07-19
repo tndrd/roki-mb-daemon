5db312b269af8521c5927dc7ec85ab91e0b783a0
Vladimir Lekhterev <vovalekhterev@gmail.com>
2024-07-19 12:48:42 +0300

    Implement handler

--- Inc/HandlerImpl.hpp
+++ Inc/HandlerImpl.hpp
@@ -1,13 +1,33 @@
 #pragma once
 
+#include <memory>
+#include <mutex>
+#include <vector>
+
 #include "Server.hpp"
 
 namespace Roki {
-class HandlerImpl final: public IHandler {
-  #define RPC RPCDefs::Procedures
+class HandlerImpl final : public IHandler {
+ private:
+  FirmwareFSM Firmware;
+  std::unique_ptr<std::mutex> Mutex;
 
-  #define PROCEDURE(Proc) RPC::Proc::Responce Proc(const RPC::Proc::Request&) override;
-  #include "Procedures.list"
-  #undef PROCEDURE
+  bool Acquired = false;
+  std::string UserName;
+  pid_t UserPid = -1;
+
+ private:
+  void EnsureNotAcquired(const std::string& prefix);
+
+ public:
+  HandlerImpl() : IHandler{}, Mutex{std::make_unique<std::mutex>()} {}
+
+#define RPC RPCDefs::Procedures
+
+#define PROCEDURE(Proc)                                 \
+  std::unique_ptr<IResponce<RPC::Proc::Responce>> Proc( \
+      const RPC::Proc::Request&) override;
+#include "Procedures.list"
+#undef PROCEDURE
 };
-}
+}  // namespace Roki
